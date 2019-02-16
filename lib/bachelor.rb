71f97a3f9a55bfb652ccaaab1b1462ac3c8b330a
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 22:08:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,9 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |x, y|
+  end
 end
 
 def get_occupation(data, hometown)
