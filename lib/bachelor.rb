ca1e96666295c097152591a4a1fe41660eeee6a2
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 22:44:24 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,52 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |section|
+    section.each do |key, value|
+      if section["status"] == "Winner"
+      return section["name"].to_s.split.first
+    end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_hash|
+    contestant_hash.each do |deets|
+    if deets["occupation"] == occupation
+      return deets["name"]
+    end
+  end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant_hash|
+    contestant_hash.each do |info|
+      if info["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_hash|
+    contestant_hash.each do |info|
+      if info["hometown"] == hometown
+        return info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = []
+  age_as_numbers = []
+  data[season].each do |deets|
+    age << deets["age"]
+  end
+  age.each{|string| age_as_numbers << string.to_i}
+  return (age_as_numbers.inject(0.0) { |sum, el| sum + el } / age_as_numbers.size).round
 end
