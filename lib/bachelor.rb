2103ceb9ff076d0e941c0fcaa2fa5ecc12eab725
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 22:23:41 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,7 +32,10 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_hash|
+    contestant_hash.each do |info|
+    end
+  end  
 end
 
 def get_average_age_for_season(data, season)
