81c5d7d4b6ac89bbcc9314c70bcf766f056caac4
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 22:15:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,8 +11,9 @@ end
 
 def get_contestant_name(data, occupation)
   data.each do |season, contestant_hash|
-    if contestant_hash["occupation"] == occupation 
+    if contestant_hash["occupation"] == occupation
       return contestant_hash["name"]
+    end
   end
 end
 
